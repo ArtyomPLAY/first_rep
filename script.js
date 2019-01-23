e52c03436d86a6c90fad132478e8b93890e2bc18
ArtyomPLAY <artglz63@gmail.com>
2019-01-23 13:22:51 +0300

    second tryings JS

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-alert("It works!");
+function changeColor(color){
+    var element = document.getElementById("topmenu");
+    element.style.backgroundColor = color;
+}
+
+window.onload=function(){
+var element = document.getElementById("topmenu");
+firstcolor = window.getComputedStyle(element).backgroundColor;
+console.log("Первый цвет:" + firstcolor + element);
+}
